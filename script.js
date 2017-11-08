94d9792fab302cfb5c91d7c3c345c1e4387c8126
UncleSnail <unclesnailproductions@gmail.com>
2017-11-08 15:48:41 -0600

    Added 'sticky' navbar and fixed blog layout issues.

--- script.js
+++ script.js
@@ -16,3 +16,47 @@ toNext = function (next) {
     behavior: 'smooth'
   });
 };
+
+//For "sticky" header;
+
+var navBar;
+var startPos;
+var startWidth;
+var yPos;
+
+window.onload = function() {
+  navBar = document.getElementById('nav');
+  startPos = nav.offsetTop;
+  startWidth = nav.offsetWidth;
+  /*Fix centering issues*/
+  nav.style.width = startWidth + 'px';
+  if (yPos >= startPos){
+    nav.style.position = 'fixed';
+  }
+
+}
+
+window.onresize = function() {
+  /*get appropriate window size
+  by setting back to correct size
+  for new window, then changing
+  to sticky again.*/
+  nav.style.position = 'relative';
+  nav.style.width = '100%';
+  startWidth = nav.offsetWidth;
+  nav.style.width = startWidth + 'px';
+  if (yPos >= startPos){
+    nav.style.position = 'fixed';
+  }
+}
+
+window.onscroll = function() {
+  yPos = window.pageYOffset;
+  if (yPos >= startPos) {
+    nav.style.position = 'fixed';
+  }
+  if (yPos < startPos) {
+    nav.style.position = 'relative';
+  }
+  nav.style.width = startWidth + 'px';
+}
