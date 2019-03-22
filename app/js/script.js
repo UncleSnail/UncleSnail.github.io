d53422a8eee9f209bf549668a648947d19515cc9
UncleSnail <caleb.marcoux@gmail.com>
2019-03-22 16:59:55 -0500

    Created working wind streak

--- app/js/script.js
+++ app/js/script.js
@@ -5,74 +5,7 @@ I solute you, my fellow programmer.
 
 //<script src="https://www.khanacademy.org/computer-programming/bird-bait-16/4568063333629952/embed.js?editor=yes&buttons=yes&author=yes&embed=yes&width=400px&height=400px"></script>
 
-toTop = function () {
-  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
-};
-
-removeWidth = function () {
-  $("p").removeAttr('style');
-}
-
-toNext = function (next) {
-  document.querySelector(next).scrollIntoView({
-    behavior: 'smooth'
-  });
-};
-
-//For "sticky" header;
-
-var navBar;
-var startPos;
-var startWidth;
-var yPos;
-
-window.onload = function() {
-  navBar = document.getElementById('nav');
-  startPos = nav.offsetTop;
-  startWidth = nav.offsetWidth;
-  /*Fix centering issues*/
-  nav.style.width = startWidth + 'px';
-  if (yPos >= startPos){
-    nav.style.position = 'fixed';
-  }
-
-}
-
-window.onresize = function() {
-  /*get appropriate window size
-  by setting back to correct size
-  for new window, then changing
-  to sticky again.*/
-  nav.style.position = 'relative';
-  nav.style.width = '100%';
-  startWidth = nav.offsetWidth;
-  nav.style.width = startWidth + 'px';
-  if (yPos >= startPos){
-    nav.style.position = 'fixed';
-  }
-}
-
-window.onscroll = function() {
-  yPos = window.pageYOffset;
-  if (yPos >= startPos) {
-    nav.style.position = 'fixed';
-  }
-  if (yPos < startPos) {
-    nav.style.position = 'relative';
-  }
-  nav.style.width = startWidth + 'px';
-}
-
 $(document).ready(function(){
-  $(".shepherd").click(function(){
-    $(this).html('<iframe src="https://www.khanacademy.org/computer-programming/planet-explorer/5870857777250304/embedded?id=1553046571320-0.8628710634770087&origin=http%3A%2F%2Fwww.calebmarcoux.com&author=yes&height=600px&buttons=yes&width=600px&editor=yes&embed=yes" style="border: 0px none; width: 1140px; height: 667px;" scrolling="no" frameborder="0">#document<!DOCTYPE html><html class="embed to-top" xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml" lang="en" style=""></html></iframe>');
-    //$(this).html('hello');
-    $("html").css("width", "");
-  });
   $('.carousel-pages').slick({
     lazyLoad: 'ondemand',
     slidesToShow: 1,
@@ -90,9 +23,4 @@ $(document).ready(function(){
     centerMode: true,
     focusOnSelect: true
   });
-
-  //document.getElementById("shepherd").innerHTML = '<iframe src="https://www.khanacademy.org/computer-programming/planet-explorer/5870857777250304/embedded?id=1553046571320-0.8628710634770087&origin=http%3A%2F%2Fwww.calebmarcoux.com&author=yes&height=600px&buttons=yes&width=600px&editor=yes&embed=yes" style="border: 0px none; width: 1140px; height: 667px;" scrolling="no" frameborder="0">#document<!DOCTYPE html><html class="embed to-top" xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml" lang="en" style=""></html></iframe>'
-
-  $('.to-top').click(toTop);
-  $("html").css("width", "");
 });
--- app/js/wind.coffee
+++ app/js/wind.coffee
@@ -3,7 +3,7 @@
 # Created by Caleb Marcoux
 # This file contains the functions to reset the wind positions and animation times/speeds for the wind effect.
 
-inView (element) ->
+inView = (element) ->
   viewTop = $(document).scrollTop()
   viewBottom = viewTop + $(window).height()
   elementTop = element.offset().top
@@ -11,20 +11,37 @@ inView (element) ->
   viewBottom > elementTop and viewTop < elementBottom
 
 $(document).ready(() ->
-  windContainer = $('#wind-container')
+  # Populate wind streaks
+
+  line = document.createElementNS("http://www.w3.org/2000/svg", 'line')
+  line.style.strokeWidth = "2px"
+  $('#wind-container').appendChild(line)
+  # $('#wind-container').append("""<line class="wind queued" x1="0" y1="100" x2="300" y2="100" style="stroke: white;stroke-width:2"/>""")
+
+  $('#wind-container line.wind').on('webkitAnimationIteration animationiteration', () ->
+    line = $(this)
+    yposition = Math.random()*200
+    line.attr({
+      x1 : "#{0}",
+      y1 : "#{yposition}",
+      x2 : "#{300}",
+      y2 : "#{yposition}",
+      })
+    length = line.getTotalLength()
+    line.css(
+      {
+        "stroke" : "yellow",
+        "stroke-dasharray" : "#{length} #{length}"
+        "stroke-dashoffset" : "#{length}"
+      });
+    return).removeClass("queued")
+
   $(document).scroll(() ->
-    if inView(windContainer)
-      $('#wind-container > line').on('webkitAnimationEnd animationend', () ->
-        line = $('.wind')
-        length = line.getTotalLength()
-        line.css(
-          {
-            "stroke" : "yellow",
-            "stroke-dasharray" : "#{length} #{length}"
-            "stroke-dashoffset" : "#{length}"
-          });
-        null)
+    if inView($('#wind-container'))
+      yposition = Math.random()*100
+      $('#wind-container line.wind.queued').css("animation-iteration-count", "infinite").removeClass("queued")
+      # Use a queued class to only select elements that do not already have a handler attached.
     else
-      $('#wind-container > line').off()
-    null)
-  null)
+      $('#wind-container > line').css("animation-iteration-count", "1").addClass("queued")
+    return)
+  return)
