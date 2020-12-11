a531a700f4527cfa97a0fe29c3d417abc86128d4
Caleb Marcoux <cmeachtech56@gmail.com>
2020-12-11 14:54:54 -0500

    Updated the projects carousel for better display on small screens.

--- app/js/script.js
+++ app/js/script.js
@@ -13,6 +13,24 @@ $(function(){
     dots: true,
     adaptiveHeight: true
   });
+
+  // Add mobile code/app switching.
+  $('.show-app').click(function() {
+    console.log("SHOW APP");
+    $('.prettyprint').addClass('hidden');
+    $('.app-wrapper').removeClass('hidden');
+    $('.app-wrapper').addClass('shown');
+    $('.prettyprint').removeClass('shown');
+  });
+  $('.show-code').click(function() {
+    console.log("SHOW CODE");
+    $('.app-wrapper').addClass('hidden');
+    $('.prettyprint').removeClass('hidden');
+    $('.prettyprint').addClass('shown');
+    $('.app-wrapper').removeClass('shown');
+  });
+
+  // Set up code previews.
   $('.code').each(function() {
     var file = $(this).attr("data-src");
     var element = $(this);
@@ -24,18 +42,17 @@ $(function(){
       }
     });
   });
-  //Only load the code prettifier after the content is read.
+  // Only load the code prettifier after the content is read.
   prettify = document.createElement("script");
   prettify.src = "https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js";
   $('head').append(prettify);
   lineNumbers = document.createElement("style");
   lineNumbers.innerHTML = '.linenums li {list-style-type: decimal;background: #fff;}ol.linenums {padding-left: 57px;}pre{white-space: pre-wrap;}';
-  //Appended to the body so that it is after the style automattically appended by prettify.
+  // Appended to the body so that it is after the style automattically appended by prettify.
   $('body').append(lineNumbers);
 });
 
 $(window).on("load", function() {
-  // Use -1 because the first slide is not an app wrapper.
   $('.carousel-pages').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
     window.frames[currentSlide].noLoop();
     window.frames[nextSlide].loop();
